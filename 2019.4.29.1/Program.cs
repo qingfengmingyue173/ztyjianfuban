442fcd989475ad356173852d9170238ec5afd044
a1bd6e26 <1401650720@qq.com>
2019-04-29 15:06:27 +0800

    添加项目文件。

---
+++ 2019.4.29.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019._4._29._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.WriteLine("love");
+			//string what = Console.ReadLine();
+
+
+			//for (int i = 0; i < 15; i++)
+			{
+				//int code = Console.Read();
+				//Console.WriteLine(code);
+				//Console.Read();
+			}
+
+			Console.WriteLine("short.ManValue：" + short.MaxValue);
+			Console.WriteLine("int.ManValue：" + int.MaxValue);
+			Console.WriteLine("long.ManValue：" + long.MaxValue);
+			Console.Read();
+		}
+	}
+}
