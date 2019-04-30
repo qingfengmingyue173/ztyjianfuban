db5ace13cb128d6242f82d138b88f76abeff12d0
a1bd6e26 <1401650720@qq.com>
2019-05-01 01:58:06 +0800

    听到作业一脸懵逼，只能剧透下一集，我也不知道是理解了还是把飞哥你的代码给背下来了。。而且还有一个BUG数值位数运行到12.13位.的时候就会超位数容量然后爆掉--

--- 2019.4.29.1/Program.cs
+++ 2019.4.29.1/Program.cs
@@ -10,35 +10,40 @@ namespace _2019._4._29._1
 	{
 		static void Main(string[] args)
 		{
-			//14课变量作业随机产生1-1000数值，10次内猜到它！
+			//实现数组
+			Console.WriteLine("数组：1, 8, 13, 14, 25, 26, 37, 68, 99, 100, 110, 123, 136, 314, 415");
+			int[] array = new int[] { 1, 8, 13, 14, 25, 26, 37, 68, 99, 100, 110, 123, 136, 314, 415 };
 
-			int max = 1000;
-			int random = new Random().Next(max);
-			Console.WriteLine($"系统会生成一个不大于{max}的数值让我们在10次内猜到它吧！");
-			for (int i = 0; i < 10; i++)
+			//实现输入
+			Console.WriteLine("请输入要查找的数；");
+			int input = Convert.ToInt32(Console.ReadLine());
+
+			//初始数组位数
+			int length = array.Length - 1;
+
+			for (int i = 0; i < array.Length; i++)
 			{
-				int import = Convert.ToInt32(Console.ReadLine());
+				int interval = (length + 1) / 2;
 
-				if (import > random)
+
+				if (input == array[length])
 				{
-					Console.WriteLine($"第{i + 1}次回答,大了");
-
-				}
-				else if (import < random)
-				{
-
-					Console.WriteLine($"第{i + 1}次回答，小了");
-
+					Console.WriteLine($"第{i}次查找，数组第{length + 1}位，数值为{array[length]}数据匹配，查找完成！");
+					break;
 				}
 				else
 				{
-					Console.WriteLine($"第{i + 1}次回答，答对了");
-					break;
+					Console.WriteLine($"第{i}次查找，数组第{length + 1}位，数值为{array[length]}");
+					if (input < array[length])
+					{
+						length -= interval;
+					}
+					else
+					{
+						length += interval;
+					}
 				}
-
-
 			}
-			Console.WriteLine($"真笨是：{random}啦");
 			Console.Read();
 
 		}
